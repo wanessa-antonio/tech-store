3d33f9280274409b7314290ab7e79113eefe5fdd
wanessa-antonio <wanessa.ant04@gmail.com>
2026-04-04 14:23:12 -0300

    chore: setup local dev servers (angular + json-server)

--- src/app/features/courses/components/course-list/course-list.component.ts
+++ src/app/features/courses/components/course-list/course-list.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseService } from '../../services/course.service';
 
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-course-list',
-  standalone: true, 
-  imports: [CommonModule], 
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
   templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
@@ -20,3 +22,4 @@ export class CourseListComponent implements OnInit {
     });
   }
 }
+
